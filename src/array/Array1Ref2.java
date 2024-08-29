b7823b64732e50f12057ff0e76bac8326efba5f2
jiho5191 <133729269+jiho5191@users.noreply.github.com>
2024-08-30 01:31:57 +0900

    Add files via upload

--- src/array/Array1Ref2.java
+++ src/array/Array1Ref2.java
@@ -1,6 +1,6 @@
 package array;
 
-public class Array1Ref1 {
+public class Array1Ref2 {
 
     public static void main(String[] args) {
         int[] students;  // 배열 변수 선언
@@ -15,10 +15,9 @@ public class Array1Ref1 {
         students[3] = 60;
         students[4] = 50;
 
-        System.out.println("학생1 점수: " + students[0]);
-        System.out.println("학생2 점수: " + students[1]);
-        System.out.println("학생3 점수: " + students[2]);
-        System.out.println("학생4 점수: " + students[3]);
-        System.out.println("학생5 점수: " + students[4]);
+        // 변수 값 사용
+        for (int i = 0; i < students.length; i++) {
+            System.out.println("학생" + (i + 1) + " 점수: " + students[i]);
+        }
     }
 }
